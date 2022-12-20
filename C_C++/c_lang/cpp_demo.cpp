25a6a250aa71e075af6031dc7d3611294eddde8e
iammrallblue <iammrallblue@gmail.com>
2022-12-20 08:58:27 -0500

    C++ notes

---
+++ C_C++/c_lang/cpp_demo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int x = 10;
+    int y = 20;
+    int z = 0;
+    cout << "10 + 20 = " << z << endl;
+    return 0;
+} // main
